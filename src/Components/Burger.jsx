bfed55df7d9fe636bdafc667bc48db06c055cac6
Antonio Grosu <antonio.webdesign19@gmail.com>
2023-03-05 18:24:36 +0200

    added links

--- src/Components/Burger.jsx
+++ src/Components/Burger.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { Link } from "react-router-dom";
 export default function Burger() {
   const [isOpen, setIsOpen] = useState(false);
   return (
@@ -19,12 +20,16 @@ export default function Burger() {
             <h1 className="text-2xl urbanist font-semibold text-gray-50">
               Menu
             </h1>
-            <p className="text-sm open-sans text-gray-50 my-6 w-full text-center py-2">
-              Dashboard
-            </p>
-            <p className="text-sm open-sans text-gray-50 w-full text-center py-2">
-              Courses
-            </p>
+            <Link to="/dashboard">
+              <p className="text-sm open-sans text-gray-50 my-6 w-full text-center py-2">
+                Dashboard
+              </p>
+            </Link>
+            <Link to="/courses">
+              <p className="text-sm open-sans text-gray-50 w-full text-center py-2">
+                Courses
+              </p>
+            </Link>
           </div>
           <div
             className="bg-gray-900 opacity-50 p-2 absolute w-2/4 left-0 top-0 rounded-l-2xl h-full"
